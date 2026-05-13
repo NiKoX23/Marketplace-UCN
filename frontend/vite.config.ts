f74c49a9bd2df78ff61f70f1603fcc8cc1099678
ElRetrillo <datrigob@gmail.com>
2026-05-13 13:49:14 -0400

    Correcion config vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: [
+      "172-16-13-104.nip.io"
+    ]
+  }
 })
