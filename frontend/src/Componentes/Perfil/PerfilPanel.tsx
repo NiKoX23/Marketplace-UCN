b9e8abb7c33a560c8d474fa3e72d8486b7c7657f
ElRetrillo <datrigob@gmail.com>
2026-05-13 11:25:32 -0400

    Modificaciones rutas

--- frontend/src/Componentes/Perfil/PerfilPanel.tsx
+++ frontend/src/Componentes/Perfil/PerfilPanel.tsx
@@ -17,7 +17,7 @@ const PerfilPanel: React.FC<PerfilPanelProps> = ({ isOpen, onClose }) => {
         if (isOpen && !perfil) {
             setCargando(true);
             const token = localStorage.getItem("accessToken");
-            fetch("http://localhost:3000/auth/perfil", {
+            fetch("http://172-16-13-104.nip.io:3000/auth/perfil", {
                 headers: {
                     "Authorization": `Bearer ${token}`
                 }
