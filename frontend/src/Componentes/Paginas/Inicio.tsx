dc084811727b425a26e13f40e0fc58f71c439522
Nicolas <nicox.lobosg@gmail.com>
2026-05-14 16:15:45 -0400

    prueba archivo

--- frontend/src/Componentes/Paginas/Inicio.tsx
+++ frontend/src/Componentes/Paginas/Inicio.tsx
@@ -57,7 +57,7 @@ function Inicio() {
     useEffect(() => {
         const fetchOfertas = async () => {
             try {
-                const res = await fetch("http://localhost:3000/publicaciones");
+                const res = await fetch("http://172-16-13-104.nip.io:3000/publicaciones");
                 if (res.ok) {
                     const data = await res.json();
                     setUltimasOfertas(data.slice(0, 5));
